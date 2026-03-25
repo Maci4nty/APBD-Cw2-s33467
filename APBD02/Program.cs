4f0c0dbe8847b8ba822b7f562374276b44925837
Maciej Kowalski <s33467@pjwstk.edu.pl>
2026-03-25 17:00:50 +0100

    Dodanie sprzętów do bazy oraz poprawienie typu użytkownika przy pracowniku

--- APBD02/Program.cs
+++ APBD02/Program.cs
@@ -7,7 +7,15 @@ using APBD02.Services;
 RentalService rentalServ = new RentalService();
 
 User stud = new User("Jan", "Kiełbasa", UserType.Student);
-User pracownik = new User("Adam", "Smakosz", UserType.Student);
+User pracownik = new User("Adam", "Smakosz", UserType.Employee);
 
 rentalServ.AddUsr(stud);
 rentalServ.AddUsr(pracownik);
+
+Laptop laptop = new Laptop("MacBook Pro", 16, "M5");
+Camera cam = new Camera("Sony Alpha 7", "CMOS", true);
+Projector projector = new Projector("XGIMI TITAN2", 10000, "1920 x 1200");
+
+rentalServ.AddEquipment(projector);
+rentalServ.AddEquipment(cam);
+rentalServ.AddEquipment(laptop);
